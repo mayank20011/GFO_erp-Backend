1768be58b28fa34fe776d7a18fe62ae43b8d9168
mayank20011 <mayank.behl.mb1@gmail.com>
2025-01-08 11:58:12 +0530

    Added new session in addNewVendor in PurchaseVendor.jsx in controller

--- controllers/PurchaseVendor.js
+++ controllers/PurchaseVendor.js
@@ -1,6 +1,7 @@
 import PurchaseVendors from "../models/PurchaseVendors.js";
 import paymentSchema from "../models/PayamentRecord.js";
 import { startSessionAndTransaction } from "../config/db.js";
+import PurchaseData from "../models/PurchaseData.js";
 
 export function getAllVendors(req, res) {
   PurchaseVendors.find()
@@ -9,24 +10,6 @@ export function getAllVendors(req, res) {
     })
     .catch((err) => {
       console.log(err);
-    });
-}
-
-export function addNewVendor(req, res) {
-  PurchaseVendors.create(req.body)
-    .then((response) => {
-      if (response) {
-        res.status(201).json({
-          success: true,
-          data: response,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(400).json({
-        success: false,
-        err: err,
-      });
     });
 }
 
@@ -48,17 +31,54 @@ export function replaceVendor(req, res, next) {
     });
 }
 
+export function addNewVendor(req, res) {
+  const { data, record } = req.body;
+
+  // Ensure vendorName is set in the record
+  if (!record.vendorName) {
+    record.vendorName = data.name || "Default Vendor Name";
+  }
+
+  const runTransactionExample = () => {
+    return startSessionAndTransaction((session) => {
+      return PurchaseVendors.create([data], { session }) // Wrap data in an array
+        .then(() => PurchaseData.create([record], { new: true, session })) // Include session in options
+        .catch((error) => {
+          throw error;
+        });
+    });
+  };
+
+  runTransactionExample()
+    .then(() => {
+      // Transaction committed, send a success response
+      res.status(200).json({
+        message: "Vendor Created Successfully",
+        success: true,
+        data: { data, record },
+      });
+    })
+    .catch((error) => {
+      // Transaction aborted or an error occurred, send an error response
+      res.status(500).json({
+        message: "Vendor Creation failed",
+        success: false,
+        error: error.message,
+      });
+    });
+}
+
 export function updateBalanceAndKeepHistory(req, res) {
   const { update, record } = req.body;
   const runTransactionExample = () => {
     return startSessionAndTransaction((session) => {
       return paymentSchema
-        .create([record], { session })  // Wrap record in an array
+        .create([record], { session }) // Wrap record in an array
         .then(() =>
           PurchaseVendors.findByIdAndUpdate(
             update._id,
             { balanceAmount: update.balanceAmount },
-            { new: true, session }  // Include session in options
+            { new: true, session } // Include session in options
           )
         );
     });
@@ -67,10 +87,18 @@ export function updateBalanceAndKeepHistory(req, res) {
   runTransactionExample()
     .then(() => {
       // Transaction committed, send a success response
-      res.status(200).json({ message: "Transaction successful", success: true, data: { update, record } });
+      res.status(200).json({
+        message: "Transaction successful",
+        success: true,
+        data: { update, record },
+      });
     })
     .catch((error) => {
       // Transaction aborted or an error occurred, send an error response
-      res.status(500).json({ message: "Transaction failed", success: false, error: error.message });
+      res.status(500).json({
+        message: "Transaction failed",
+        success: false,
+        error: error.message,
+      });
     });
 }
